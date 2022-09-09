cd074bced0ecf35ee89f6e2e3c001449d481e5ba
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-09-09 10:03:59 +0530

    chnSN

--- 8cardGame/main.mjs
+++ 8cardGame/main.mjs
@@ -4,8 +4,8 @@ import { loadAssets } from "./js/loader.mjs";
 
 export function createGame(div) {
     const game = new Application({
-        width:1024,
-        height:920,
+        width:innerWidth,
+        height:innerHeight,
         backgroundColor:0x2266FF
     });
     console.log('game', game);
