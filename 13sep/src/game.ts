ee338b0def6a9d34bf1b764c57eea969cac6b42c
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-09-14 15:50:25 +0530

    chnagwa

---
+++ 13sep/src/game.ts
@@ -0,0 +1,45 @@
+import {Application, Sprite} from 'pixi.js';
+import {gsap} from 'gsap';
+export class Game extends Application{
+    private spin:boolean;
+    private sliceAngle = 360/7;
+    constructor(opts:any) {
+        super(opts);
+        this.preload([
+            
+            {name:'wheel', url:'assets/wheel1.png'},
+            {name:'spin',url:'assets/arrow.png'}
+           
+        ], this.onLoad.bind(this));
+    }
+    preload(list:any[], cb:()=>{}):void {
+        this.loader.add(list);
+        this.loader.load(cb);
+    }
+    onLoad():void {
+        const wheel = new Sprite(this.loader.resources['wheel'].texture);
+        const spin1 =new Sprite(this.loader.resources['spin'].texture);
+        wheel.anchor.set(0.5);
+        wheel.x = this.screen.width/2;
+        wheel.y = this.screen.height/2;
+
+         spin1.anchor.set(0.5);
+         spin1.x=this.screen.width/2+5;
+         spin1.y=this.screen.height/2 +10;
+         spin1.width=50;
+         spin1.height=50;
+         
+        this.stage.addChild(wheel);
+
+        this.stage.addChild(spin1);
+
+        spin1.interactive = true;
+        spin1.buttonMode = true;
+        console.log(this.stage);
+        spin1.on('pointerup', ()=>{
+            let random = Math.floor(Math.random()*7);
+            let stopAngle = random * this.sliceAngle;
+            gsap.fromTo(spin1,{angle:0},{angle:3600+stopAngle, duration:3, ease:'slow(0.9, 0.9, false)'});
+        });
+    }
+}
--- 13sep/src/index.ts
+++ 13sep/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import { Application } from 'pixi.js';
+import { Game } from './Game';
+import './css/main.css';
+window.onload = ()=>{
+    const gameDiv:HTMLDivElement = <HTMLDivElement>document.getElementById('game');
+    const app:Application = new Game({
+        width:innerWidth,
+        height:innerHeight,
+        //resizeTo: gameDiv,
+        backgroundColor: 0x996633,
+        sharedLoader: true,
+        sharedTicker: true
+    });
+    gameDiv.appendChild(app.view);
+}
