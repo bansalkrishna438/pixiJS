2365eb9edad31765c2f8ce7df8758eed66eba50e
bansalkrishna438 <bansalkrishna438@gmail.com>
2022-09-16 16:20:52 +0530

    chnagws

--- assignment3/main.mjs
+++ assignment3/main.mjs
@@ -15,7 +15,7 @@ export function createGame(div) {
 
 const game = createGame(document.getElementById('game'));
 loadAssets(game,[
-    { name: "back", url: "assets/card.png" },
+    { name: "back", url: "assets/cardback.png" },
     { name: "front", url: "assets/card.png" },
   ], () => {
     console.log('starting game');
